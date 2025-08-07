86527e701be1dddeb0949dac0423a9f9b51d6bea
emcnicholas <edmcnich@cisco.com>
2025-08-07 07:33:37 -0400

    Create env-vars.sh

---
+++ intro/terraform/env-vars.sh
@@ -0,0 +1,7 @@
+### Add your Security Cloud Control and Cloud Delivered FMC information here
+
+ export TF_VAR_cdo_token="ADD YOUR TOKEN" #This is your Security Cloud Control API Token.
+ export TF_VAR_cdFMC="ADD YOUR cdFMC HOSTNAME" #This is the hostname of the Cloud Delivered FMC.
+ export TF_VAR_name="YOURNAME"
+
+ ### Import these environment variables by running "source env-vars.sh" from your terminal.
